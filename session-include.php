2cf233ab488a82388f96bf848789a9771a0861e9
Brett Spencer <bspencer411@gmail.com>
2015-07-08 15:37:06 -0700

    this is what i was toying with
    i need to add the full $PAGE_THIS BASENAME line. if you logged in, it drops right through. if not the form shows up but i need to fix the logic of the inner if loop. i think we have the rest though. then we can drop this include in to any page header.

--- session-include.php
+++ session-include.php
@@ -1,4 +1,19 @@
 <?php
 //session-include.php
 
+$PAGE_THIS[BASENAME()];
 session_start();
+$Password="abc123";
+        
+if (!isset($_SESSION['Password'])) {
+    if (isset($_SESSION['$_POST'])) {
+        ($_SESSION['Password']) == $Password;
+        die;
+    }
+} {
+        echo '<h1>Please enter your login credentials: abc123</h1>
+                <form action="formhandler.php" method="post">
+                   Password: <br><input type="text" name="password">
+                   <input type="submit" value="submit">
+                </form>' ;       
+}
